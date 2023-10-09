b187d85ef85fb51e34a258c57ce425ec0ecef15a
hyodogabriela <gahyodo@gmail.com>
2023-10-09 17:28:27 +0200

    Adjusted the form in HTML and added functions on js
    Added search engine function and get current location function

--- src/app.js
+++ src/app.js
@@ -99,7 +99,7 @@ function showLocation(position) {
 
   }
   
-  function displayTemperature(response) {
+function displayTemperature(response) {
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind-speed");
@@ -118,11 +118,34 @@ function showLocation(position) {
     );
     iconElement.setAttribute("alt", response.data.condition.description);
   }
-  
-  let apiKey = "f0308t4943329c9be1off0f74f2fa59a";
-  let city = "Lisbon";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(displayTemperature);
+
+function search (city) {
+    let apiKey = "f0308t4943329c9be1off0f74f2fa59a";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   
- 
+    axios.get(apiUrl).then(displayTemperature);
+}
+  
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#search-text-input");
+    search(cityInputElement.value);
+  }
+
+    let form = document.querySelector("#search-form");
+    form.addEventListener("submit", handleSubmit);
+
+function showLocation(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let apiKey = "f0308t4943329c9be1off0f74f2fa59a";
+    let units = "metric";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(displayTemperature);
+  }
+  function getCurrentPosition() {
+    navigator.geolocation.getCurrentPosition(showLocation);
+  }
+  let button = document.querySelector("button");
+  button.addEventListener("click", getCurrentPosition);
