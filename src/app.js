d583e7eca96c30de35030bf5de8282e8279472bc
hyodogabriela <gahyodo@gmail.com>
2023-10-09 16:12:12 +0200

    Adjusted some bug on index and added time on js

--- src/app.js
+++ src/app.js
@@ -73,20 +73,48 @@ function showLocation(position) {
   let button = document.querySelector("button");
   button.addEventListener("click", getCurrentPosition);
  */
-   function displayTemperature(response) {
+   
+  function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ];
+    let day = days[date.getDay()];
+
+    return `${day}, ${hours}:${minutes}`;
+
+  }
+  
+  function displayTemperature(response) {
     console.log(response.data);
     let descriptionElement = document.querySelector("#description");
-    descriptionElement.innerHTML = response.data.condition.description;
     let humidityElement = document.querySelector("#humidity");
-    humidityElement.innerHTML = response.data.temperature.humidity;
     let windElement = document.querySelector("#wind-speed");
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-   /*  let iconElement = document.querySelector("#weather-icon");
+    /*  let iconElement = document.querySelector("#weather-icon");
     iconElement.innerHTML = response.data.condition.icon.; */
     let cityName = document.querySelector("#city-name");
-    cityName.innerHTML = response.data.city;
     let temperatureElement = document.querySelector("#temperature-display");
+    let dateElement = document.querySelector("#date-time");
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = response.data.temperature.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    cityName.innerHTML = response.data.city;
+    dateElement.innerHTML = formatDate(response.data.time * 1000)
   }
   
   let apiKey = "f0308t4943329c9be1off0f74f2fa59a";
